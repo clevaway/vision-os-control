928c7ef601cee8fc20c6fd1c47b98a72bcb0513f
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2024-01-23 20:57:55 +0300

    implementing in object oriented way

--- main.py
+++ main.py
@@ -3,23 +3,33 @@ import mediapipe as mp
 import pyautogui
 import math
 
+# Initialize the webcam
 cam = cv2.VideoCapture(0)
+
+# Initialize the face mesh model
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+
+# Initialize the hand tracking model
 hands = mp.solutions.hands.Hands(static_image_mode=False, max_num_hands=2,
                                  min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
+# Get the screen size
 screen_width, screen_height = pyautogui.size()
 
 while True:
+    # Read a frame from the webcam
     _, frame = cam.read()
-    # flip the frame
+
+    # Flip the frame horizontally
     frame = cv2.flip(frame, 1)
+
+    # Convert the frame to RGB format
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Process frame for hand landmarks
+    # Process the frame for hand landmarks
     hand_results = hands.process(rgb_frame)
 
-    # Draw hand landmarks
+    # Draw hand landmarks on the frame
     if hand_results.multi_hand_landmarks:
         for hand_landmarks in hand_results.multi_hand_landmarks:
             mp.solutions.drawing_utils.draw_landmarks(
@@ -34,28 +44,35 @@ while True:
                                  ** 2 + (thumb_tip.y - index_tip.y)**2)
 
             print(distance)
+
             # If the distance is less than a certain threshold, print "Pinch gesture detected"
-            if distance < 0.05:
+            if distance < 0.03:
                 print("Pinch gesture detected")
-                # click
+                # Perform a click action
                 pyautogui.click()
 
+    # Process the frame for face landmarks
     output = face_mesh.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
     frame_height, frame_width, _ = frame.shape
+
     if landmark_points:
         landmarks = landmark_points[0].landmark
         for id, landmark in enumerate(landmarks[474:478]):
             x = int(landmark.x * frame_width)
             y = int(landmark.y * frame_height)
             cv2.circle(frame, (x, y), 3, (0, 255, 0))
+
             if id == 1:
+                # Map the landmark coordinates to the screen coordinates
                 screen_x = screen_width / frame_width * x
                 screen_y = screen_height / frame_height * y
                 pyautogui.moveTo(screen_x, screen_y)
 
-    cv2.imshow("Vison Pointer", frame)
-    # Check if 'q' key is pressed
+    # Display the frame with annotations
+    cv2.imshow("Vision Pointer", frame)
+
+    # Check if 'q' key is pressed to exit the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
